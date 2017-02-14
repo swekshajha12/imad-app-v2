6feb8656a917766dcbe84b450b8e95842c510c63
Sweksha <jhasweksha12@gmail.com>
2017-02-14 17:58:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
 });
  
 app.get('/article1',function(req,res){
-   res.send("article one will be served here")});
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+});
 
 app.get('/article2',function(req,res){
    res.send("article two will be served here")});
