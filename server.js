cd43a67745527443d6869f8ae10121f86afbaa2a
Sweksha <jhasweksha12@gmail.com>
2017-03-17 12:26:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/article1',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    return counter;
+    
+});
+
 app.get('/article2',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article2.html')); 
 });
